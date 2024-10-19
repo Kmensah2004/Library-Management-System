dd52d7254c179adb323a31a05687effd81eda5e9
Kosi Mensah <kmensah@usf.edu>
2024-10-19 17:39:35 -0400

    Create Section class to manage books and availability

--- library_management_system.js
+++ library_management_system.js
@@ -22,3 +22,27 @@ class Book {
         this._isAvailable = status;
     }
 }
+// Task 2: Define the Section Class
+class Section {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    // add a book to the section
+    addBook(book) {
+        this.books.push(book);
+    }
+
+    // Method to get the count of available books 
+    getAvailableBooks() {
+        return this.books.filter(book => book.isAvailable).length;
+    }
+
+    // list all books in the section
+    listBooks() {
+        this.books.forEach(book => {
+            console.log(`${book.getDetails()}  Available: ${book.isAvailable}`);
+        });
+    }
+}
